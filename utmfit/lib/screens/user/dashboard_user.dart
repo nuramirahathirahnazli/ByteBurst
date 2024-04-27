87b516743cc508757139307c2b0bceffc34a6433
Wan Muhammad Aiman <aiman.eduprogram@gmail.com>
2024-04-28 03:57:43 +0800

    wan

--- utmfit/lib/screens/user/dashboard_user.dart
+++ utmfit/lib/screens/user/dashboard_user.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:utmfit/screens/user/profile/profile_user.dart';
 import 'package:utmfit/screens/user/Auth/signin_user.dart';
 import 'package:utmfit/src/constants/colors.dart';
-import 'package:utmfit/src/constants/colors.dart';
 
 class dashboardUser extends StatefulWidget {
   const dashboardUser({super.key});
@@ -68,82 +67,171 @@ class _dashboardUserState extends State<dashboardUser> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           // Choice of facilities section
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'Choice a Facility:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Add logic to navigate to the Squash facility page
-                  },
-                  icon: Image.asset('assets/images/squash.png'),
-                  label: Text('Squash'),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Color.fromARGB(255, 255, 231, 172)),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
-                  ),
+          Container(
+            padding: EdgeInsets.only(top: 10.0), // Add padding only at the top
+            child: Card(
+              margin: EdgeInsets.all(8.0),
+              elevation: 4.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Text(
+                      'Choose a Facility:',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        // Add logic to navigate to the Squash facility page
+                      },
+                      icon: Image.asset(
+                        'assets/images/squash.png',
+                        width: 40,
+                        height: 30,
+                      ),
+                      label: Text(
+                        'Squash',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 255, 231, 172),
+                        ),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                          Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0), // Add spacing between the buttons
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        // Add logic to navigate to the Ping Pong facility page
+                      },
+                      icon: Image.asset(
+                        'assets/images/pingpong.png',
+                        width: 30,
+                        height: 30,
+                      ),
+                      label: Text(
+                        'Ping Pong',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 255, 231, 172),
+                        ),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                          Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0), // Add spacing between the buttons
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        // Add logic to navigate to the Badminton facility page
+                      },
+                      icon: Image.asset(
+                        'assets/images/badminton.png',
+                        width: 30,
+                        height: 30,
+                      ),
+                      label: Text(
+                        'Badminton',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 255, 231, 172),
+                        ),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                          Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Add logic to navigate to the Ping Pong facility page
-                  },
-                  icon: Image.asset('assets/images/pingpong.png'),
-                  label: Text('Ping Pong'),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Color.fromARGB(255, 255, 231, 172)),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
-                  ),
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Add logic to navigate to the Badminton facility page
-                  },
-                  icon: Image.asset('assets/images/badminton.png'),
-                  label: Text('Badminton'),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Color.fromARGB(255, 255, 231, 172)),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
 
           // List of announcements
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'Announcements:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Expanded(
-            child: ListView(
-              children: <Widget>[
-                ListTile(
-                  title: const Text('Announcement 1'),
-                  subtitle: const Text('Details of Announcement 1'),
-                ),
-                ListTile(
-                  title: const Text('Announcement 2'),
-                  subtitle: const Text('Details of Announcement 2'),
-                ),
-                // Add more announcements as needed
-              ],
+          Padding(
+            padding: const EdgeInsets.only(top: 4.0), // Add padding at the top
+            child: Card(
+              margin: EdgeInsets.all(8.0),
+              elevation: 4.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Announcements:',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ),
+                  Divider(),
+                  ListView(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(
+                          'Important Announcement',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                        ),
+                        subtitle: Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod dui sed nisi pretium, eget suscipit risus laoreet.',
+                          style: TextStyle(color: Colors.black54),
+                        ),
+                        leading: Icon(
+                          Icons.notifications,
+                          color: Color(0xFF9F4F5D),
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Color(0xFF9F4F5D),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                          'Reminder: Maintenance',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                        ),
+                        subtitle: Text(
+                          'Closed due to maintenance',
+                          style: TextStyle(color: Colors.black54),
+                        ),
+                        leading: Icon(
+                          Icons.notifications,
+                          color: Color(0xFF9F4F5D),
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Color(0xFF9F4F5D),
+                        ),
+                      ),
+                      // Add more announcements as needed
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
--- utmfit/lib/src/constants/image_strings.dart
+++ utmfit/lib/src/constants/image_strings.dart
@@ -4,6 +4,3 @@
 // -- Profile Images
 const String strProfileImage = "assets/images/profile_pic.jpg";
 const String strProfileImage2 = "assets/images/profile_pic2.jpg";
-const String BadmintonIcon = "assets/images/badminton.png";
-const String PingPongIcon = "assets/images/pingpong.png";
-const String SquashIcon = "assets/images/squash.png";

a9d76362f7857c91acdc9a9ebe45015fdc71574b
Muhd Afnan <88524435+zeynanz@users.noreply.github.com>
2024-04-28 22:18:01 +0800

    Sign Up Sign In Firebase Fucntion
    Ada tambah configuration sikit

--- utmfit/lib/main.dart
+++ utmfit/lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:utmfit/screens/user/dashboard_user.dart'; //dashboard user purposes
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
